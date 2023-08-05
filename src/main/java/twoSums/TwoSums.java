7fbb00f8dbdcd79ebb2ee43916c0af937daca618
Elina1313 <112032648+Elina1313@users.noreply.github.com>
2023-08-05 14:09:57 +0300

    feat: find Median Sorted Arrays

--- src/main/java/twoSums/TwoSums.java
+++ src/main/java/twoSums/TwoSums.java
@@ -1,7 +1,7 @@
 package twoSums;
 
 public class TwoSums {
-    public static void main(String[] args) {
+    public static void main(String[] args) { //
         Solution solution = new Solution();
         solution.twoSum(new int[]{2, 7, 11, 15}, 9);
     }
